f3da25821e7b17c3b866d0a1f814d7b3e1fe13fa
Jan Teske <jteske@posteo.net>
2020-04-17 19:40:17 +0200

    Make the DMA trait prototypes less generic

--- src/traits.rs
+++ src/traits.rs
@@ -21,14 +21,14 @@ use stable_deref_trait::StableDeref;
 /// - It must be a pointer that references the actual buffer.
 /// - The requirements documented on `dma_read_buffer` must be fulfilled.
 pub unsafe trait DmaReadBuffer {
-    type Target: ?Sized;
+    type Word;
 
     /// Provide a buffer usable for DMA reads.
     ///
     /// The return value is:
     ///
     /// - pointer to the start of the buffer
-    /// - buffer size in bytes
+    /// - buffer size in words
     ///
     /// # Safety
     ///
@@ -36,7 +36,7 @@ pub unsafe trait DmaReadBuffer {
     ///   times.
     /// - The memory specified by the returned pointer and size must be fully
     ///   readable by the DMA peripheral.
-    fn dma_read_buffer(&self) -> (*const Self::Target, usize);
+    fn dma_read_buffer(&self) -> (*const Self::Word, usize);
 }
 
 /// Trait for buffers that can be given to DMA for writing.
@@ -49,14 +49,14 @@ pub unsafe trait DmaReadBuffer {
 /// - `Target` must be a type that is valid for any possible byte pattern.
 /// - The requirements documented on `dma_write_buffer` must be fulfilled.
 pub unsafe trait DmaWriteBuffer {
-    type Target: ?Sized;
+    type Word;
 
     /// Provide a buffer usable for DMA writes.
     ///
     /// The return value is:
     ///
     /// - pointer to the start of the buffer
-    /// - buffer size in bytes
+    /// - buffer size in words
     ///
     /// # Safety
     ///
@@ -64,89 +64,99 @@ pub unsafe trait DmaWriteBuffer {
     ///   times.
     /// - The memory specified by the returned pointer and size must be fully
     ///   writable by the DMA peripheral.
-    fn dma_write_buffer(&mut self) -> (*mut Self::Target, usize);
+    fn dma_write_buffer(&mut self) -> (*mut Self::Word, usize);
 }
 
-/// Deref target for the `Dma{Read,Write}Buffer` types used by the blanket
-/// implementations.
+/// Trait for DMA word types used by the blanket implementations.
 ///
 /// # Safety
 ///
 /// Types that implement this trait must be valid for every possible byte
 /// pattern. This is to ensure that, whatever DMA writes into the buffer,
 /// we won't get UB due to invalid values.
-pub unsafe trait DmaTarget {}
+pub unsafe trait DmaWord {}
 
-unsafe impl DmaTarget for u8 {}
-unsafe impl DmaTarget for u16 {}
-unsafe impl DmaTarget for u32 {}
-unsafe impl DmaTarget for u64 {}
-unsafe impl DmaTarget for usize {}
+unsafe impl DmaWord for u8 {}
+unsafe impl DmaWord for u16 {}
+unsafe impl DmaWord for u32 {}
 
-unsafe impl DmaTarget for i8 {}
-unsafe impl DmaTarget for i16 {}
-unsafe impl DmaTarget for i32 {}
-unsafe impl DmaTarget for i64 {}
-unsafe impl DmaTarget for isize {}
+/// Trait for `DerefMut` targets used by the blanket `DmaWriteBuffer` impl.
+///
+/// This trait exists solely to work around
+/// https://github.com/rust-lang/rust/issues/20400.
+///
+/// # Safety
+///
+/// - `as_dma_write_buffer` must adhere to the safety requirements
+///   documented for `DmaWriteBuffer::dma_write_buffer`.
+pub unsafe trait DmaWriteTarget {
+    type Word: DmaWord;
 
-unsafe impl<T: DmaTarget> DmaTarget for [T] {}
-unsafe impl<T: DmaTarget> DmaTarget for MaybeUninit<T> {}
+    fn as_dma_write_buffer(&mut self) -> (*mut Self::Word, usize);
+}
 
-macro_rules! write_target_array_impls {
+unsafe impl<W: DmaWord> DmaWriteTarget for [W] {
+    type Word = W;
+
+    fn as_dma_write_buffer(&mut self) -> (*mut Self::Word, usize) {
+        (self.as_mut_ptr(), self.len())
+    }
+}
+
+macro_rules! dma_write_target_array_impls {
     ( $( $i:expr, )+ ) => {
         $(
-            unsafe impl<T: DmaTarget> DmaTarget for [T; $i] {}
+            unsafe impl<W: DmaWord> DmaWriteTarget for [W; $i] {
+                type Word = W;
+
+                fn as_dma_write_buffer(&mut self) -> (*mut Self::Word, usize) {
+                    (self.as_mut_ptr(), $i)
+                }
+            }
         )+
     };
 }
 
 #[rustfmt::skip]
-write_target_array_impls!(
+dma_write_target_array_impls!(
      0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
     10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
     30, 31, 32,
 );
 
-// Ideally we would write this blanket impl based on `AsRef`, to not be
-// limited to slices. I.e.:
-//
-//     unsafe impl<B, T> DmaReadBuffer for B
-//     where
-//         B: Deref + StableDeref,
-//         B::Target: AsRef<T>,
-//         T: DmaTarget + ?Sized,
-//     { ... }
-//
-// Rust refuses to compile that though ("unconstrained type parameter").
+unsafe impl<T: DmaWriteTarget> DmaWriteTarget for MaybeUninit<T> {
+    type Word = T::Word;
 
-unsafe impl<B, E> DmaReadBuffer for B
+    fn as_dma_write_buffer(&mut self) -> (*mut Self::Word, usize) {
+        let ptr = self.as_mut_ptr() as *mut Self::Word;
+        let len = mem::size_of::<T>() / mem::size_of::<T::Word>();
+        (ptr, len)
+    }
+}
+
+unsafe impl<B, W> DmaReadBuffer for B
 where
     B: Deref + StableDeref,
-    B::Target: AsSlice<Element = E>,
-    E: DmaTarget,
+    B::Target: AsSlice<Element = W>,
+    W: DmaWord,
 {
-    type Target = [E];
+    type Word = W;
 
-    fn dma_read_buffer(&self) -> (*const Self::Target, usize) {
-        let target = self.as_slice();
-        let ptr = target as *const Self::Target;
-        let len = mem::size_of_val(target);
-        (ptr, len)
+    fn dma_read_buffer(&self) -> (*const Self::Word, usize) {
+        let slice = self.as_slice();
+        (slice.as_ptr(), slice.len())
     }
 }
 
 unsafe impl<B, T> DmaWriteBuffer for B
 where
     B: DerefMut<Target = T> + StableDeref,
-    T: DmaTarget + ?Sized,
+    T: DmaWriteTarget + ?Sized,
 {
-    type Target = T;
+    type Word = T::Word;
 
-    fn dma_write_buffer(&mut self) -> (*mut Self::Target, usize) {
-        let target = self.deref_mut();
-        let ptr = target as *mut Self::Target;
-        let len = mem::size_of_val(target);
-        (ptr, len)
+    fn dma_write_buffer(&mut self) -> (*mut Self::Word, usize) {
+        self.as_dma_write_buffer()
     }
 }
